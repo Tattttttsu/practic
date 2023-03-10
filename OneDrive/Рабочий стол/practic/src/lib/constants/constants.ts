40fb02cff02ddd8f3e1b2d86c0763ae75eed260d
Tattttttsu <iliinyh@mail.ru>
2023-03-10 23:59:49 +0500

    update

--- OneDrive/Рабочий стол/practic/src/lib/constants/constants.ts
+++ OneDrive/Рабочий стол/practic/src/lib/constants/constants.ts
@@ -1,20 +1,20 @@
 export enum COLORS {
     TEXT_COLOR = "#FFFFFF",
-    H1 = "lightblue",
-    BACKGROUND_COLOR = "#11283d",
+    H1 = "#FFFFFF",
+    BACKGROUND_COLOR = "#ea8331",
     BACKGROUND_COLOR_TWO = "rgba(0, 0, 0, 0.3)",
     BACKDOOR_COLOR = "rgba(0, 0, 0, 0.5)",
     CODE_BACKGROUND = "rgba(0, 0, 0, 0.5)",
     LI_COLOR = "#d7d7d7",
-    BORDER_COLOR = "lightblue"
+    BORDER_COLOR = "GRAY"
 }
 
 export const BUTTONS_ARRAY = [
-    {title: "Введение в HTML", path: "html"},
-    {title: "Введение в CSS", path: "css"},
-    {title: "Введение в JS", path: "js"},
+    {title: "Введение в SQL", path: "sql"},
+    {title: "Введение в PYTHON", path: "python"},
+    {title: "Введение в Django", path: "django"},
 ];
 
 
-export const PROFESSION = `Введение в профессию Frontend разработчика`
+export const PROFESSION = `Введение в профессию Backend разработчика`
 export const IMAGE_PATH_NAME = `profession.png`
